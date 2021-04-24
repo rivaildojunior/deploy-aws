878c33cd757c91ce75232e3f14d258e63d141805
rivaildojunior <59898958+rivaildojunior@users.noreply.github.com>
2021-04-24 14:07:41 -0300

    Update DeployController.java

--- src/main/java/com/example/demodeployaws/controller/DeployController.java
+++ src/main/java/com/example/demodeployaws/controller/DeployController.java
@@ -10,6 +10,6 @@ public class DeployController {
 
 	@GetMapping
 	public String fazerDeploy() {
-		return "Realizando deploy na AWS com Jenkins";
+		return "Realizando deploy na AWS com Jenkins 2";
 	}
 }
