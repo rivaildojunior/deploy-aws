d76dd7bcf8a6b08a93eaf271e96c35a2378c5e59
Rivaildo Sousa <rivaildojunior@yahoo.com.br>
2021-04-24 12:39:45 -0300

    primeiro commit

---
+++ src/test/java/com/example/demodeployaws/DemoDeployAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodeployaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDeployAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
